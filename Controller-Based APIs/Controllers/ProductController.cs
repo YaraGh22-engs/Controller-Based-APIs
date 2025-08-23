d4dcdc428b2d090660cd5cdcc63663c4434aabc7
YaraGh <yaraesmaiel123@gmail.com>
2025-08-23 13:28:31 -0500

    Add project files.

---
+++ Controller-Based APIs/Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Controller_Based_APIs.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return " Product #1 , Price $2.99 ";
+        }
+    }
+}
