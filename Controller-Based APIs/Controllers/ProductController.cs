e91855ce8d50480023b80b5c3e4886aaf41e3073
YaraGh <yaraesmaiel123@gmail.com>
2025-08-24 06:23:28 -0500

    HEAD Endpoint

--- Controller-Based APIs/Controllers/ProductController.cs
+++ Controller-Based APIs/Controllers/ProductController.cs
@@ -1,17 +1,28 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Controller_Based_APIs.Data;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Controller_Based_APIs.Controllers
 {
     [Route("api/products")]
     [ApiController]
-    public class ProductController : ControllerBase
+    public class ProductController(ProductRepository repository) : ControllerBase
     {
+        
+
         [HttpOptions]
         public IActionResult OptionsProducts()
         {
             Response.Headers.Append("Allow", "GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS");
             return NoContent();
         }
+
+  
+        [HttpHead("{productId:guid}")]
+        public IActionResult HeadProduct(Guid productId)
+        {
+            return repository.ExistsById(productId) ? Ok() : NotFound();
+        }
+
     }
 }
